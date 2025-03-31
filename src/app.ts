fb94c894862c6f06687044fa0f9f5fb0eb27b498
rockydey <rockydey077@gmail.com>
2025-03-31 09:33:21 +0600

    validate request function and catchAsync function creared

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from SPORTS FACILITY BOOKING PLATFORM SERVER!');
---
+++ src/app/modules/User/user.validation.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+const createUserSchemaValidation = z.object({
+  body: z.object({
+    name: z.string({ required_error: 'Name is required!' }),
+    email: z.string({ required_error: 'Email is required!' }).email(),
+    password: z
+      .string({
+        required_error: 'Password is required!',
+        invalid_type_error: 'Password must be string!',
+      })
+      .min(8, { message: 'Password must be at least * character long!' }),
+    phone: z.string({ required_error: 'Phone is required!' }),
+    role: z.enum(['admin', 'user'], { required_error: 'Role is required' }),
+    address: z.string({ required_error: 'Address is required!' }),
+  }),
+});
+
+export const UserValidation = {
+  createUserSchemaValidation,
+};
